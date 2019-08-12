bad119e055e48c164704eb497c7ad5971d4cc9e4
celine lemenn <celinelemenn@gmail.com>
2019-08-12 15:10:55 +0200

    done

---
+++ test/system/questions_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  test 'visiting /ask render the form' do
+    visit ask_url
+
+    assert_selector 'p', text: 'Ask your coach anything'
+  end
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_button 'Submit'
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
